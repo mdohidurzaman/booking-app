c1701d3e3f21a0a5a8e40d1a115887ccbd9eb6dc
Md. Ohidurzaman <mozaman.knit@gmail.com>
2022-07-08 16:49:36 +0600

    added navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -3,8 +3,14 @@ import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>This is navbar</h1>
+    <div className="navbar">
+      <div className="navContainer">
+        <span className="log">Booking App</span>
+        <div className="navItems">
+          <button className="navButton">Register</button>
+          <button className="navButton">Login</button>
+        </div>
+      </div>
     </div>
   );
 };
